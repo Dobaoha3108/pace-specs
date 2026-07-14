2ee65d968f75f9b523320b927daae0893bbf598c
Dobaoha3108 <dobaoha.work123@gmail.com>
2026-07-14 14:38:11 +0700

    Update voucher-card.tsx

--- src/components/reward/voucher-card.tsx
+++ src/components/reward/voucher-card.tsx
@@ -66,7 +66,7 @@ export function VoucherCard({
 
   {/* Cột số coin và nút đổi bên phải */}
   <div className="flex shrink-0 flex-col items-center justify-center gap-0">
- <div className="flex items-center gap-0">
+<div className="-translate-y-1 flex items-center gap-0">
   <span className="text-[22px] font-extrabold leading-none text-[#F21383]">
     {pigCoinCost}
   </span>
